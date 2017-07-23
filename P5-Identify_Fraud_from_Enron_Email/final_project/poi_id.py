845c6d69121abda2bb1c4e498338f50c537b6c0a
Darren Chiu <chiusw2003@gmail.com>
2017-07-23 12:21:19 +0800

    update solution of P5

--- P5-Identify_Fraud_from_Enron_Email/final_project/poi_id.py
+++ P5-Identify_Fraud_from_Enron_Email/final_project/poi_id.py
@@ -6,7 +6,8 @@ import pickle
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
+from sklearn.cross_validation import train_test_split
+from tester import dump_classifier_and_data, test_classifier
 from sklearn.svm import SVC
 from sklearn import tree
 import numpy as np
@@ -14,31 +15,30 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn import model_selection
 from sklearn.naive_bayes import GaussianNB
 
-
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 sorted_features = [
-    'total_stock_value',
     'exercised_stock_options',
+    'expenses',
     'deferred_income',
-    'from_this_person_to_poi',
-    'restricted_stock',
     'other',
+    'restricted_stock',
+    'total_stock_value',
+    'from_poi_to_this_person',
     'long_term_incentive',
     'total_payments',
-    'number_of_missing_fields',
     'salary',
-    'bonus',
-    'shared_receipt_with_poi',
-    'expenses',
-    'from_messages',
-    'from_poi_to_this_person',
     'to_messages',
+    'bonus',
+    'number_of_missing_fields',
+    'shared_receipt_with_poi',
+    'from_this_person_to_poi',
     'deferral_payments',
+    'loan_advances',
+    'from_messages',
     'restricted_stock_deferred',
-    'director_fees',
-    'loan_advances'
+    'director_fees'
 ]
 features_list = ['poi'] + sorted_features[0:3]
 features_list_without_poi = sorted_features[0:3]
@@ -52,8 +52,6 @@ with open("final_project_dataset.pkl", "r") as data_file:
 del data_dict['TOTAL']
 del data_dict['LOCKHART EUGENE E']
 del data_dict['THE TRAVEL AGENCY IN THE PARK']
-del data_dict['SKILLING JEFFREY K']
-del data_dict['LAY KENNETH L']
 
 ### Task 3: Create new feature(s)
 for person in data_dict.keys():
@@ -95,20 +93,22 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import StratifiedShuffleSplit
 
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+    train_test_split(features, labels, test_size=0.3, random_state=42, stratify=labels)
 
 # Build the clf
-gaussian_clf = GaussianNB()
-gaussian_clf.fit(features_train, labels_train)
-print gaussian_clf.score(features_test, labels_test)
-print precision_score(labels_test, gaussian_clf.predict(features_test), average='weighted')
+clf = GaussianNB()
+# clf = tree.DecisionTreeClassifier(max_features=3, min_samples_split=2, criterion='entropy', max_depth=8, splitter='random')
+# clf = SVC(kernel='rbf', C=3000)
+
+clf.fit(features_train, labels_train)
+test_classifier(clf, my_dataset, features_list)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(gaussian_clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
